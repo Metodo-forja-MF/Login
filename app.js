94bbcd46b2c5620ff3d9e834a8969a3b82ffc38d
MF <bertiwesley15@gmail.com>
2025-07-04 13:17:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Inicialização do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBKtKrRP4AjLHcdeEdyTlTZC9hHdv5y7no",
   authDomain: "metodo-forja.firebaseapp.com",
-  databaseURL: "https://metodo-forja-default-rtdb.firebaseio.com",
   projectId: "metodo-forja",
-  storageBucket: "metodo-forja.firebasestorage.app",
+  storageBucket: "metodo-forja.appspot.com",
   messagingSenderId: "949719386034",
-  appId: "1:949719386034:web:5c015b5fbe082821ea86ab",
-  measurementId: "G-6Z9EJLY5LZ"
+  appId: "1:949719386034:web:5c015b5fbe082821ea86ab"
 };
 
-// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Função de login
-document.getElementById("login-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  
+// Lógica de login
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Evita o recarregamento da página
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(() => {
-      // Redireciona após login bem-sucedido
+      // Redireciona para a área de membros após login bem-sucedido
       window.location.href = "https://metodo-forja-mf.github.io/membros/";
     })
     .catch((error) => {
