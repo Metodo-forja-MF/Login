584989c10d596d6177130942238a729ad7da1629
MF <bertiwesley15@gmail.com>
2025-07-04 12:43:18 -0300

    Update app 2.js

--- app 2.js
+++ app 2.js
@@ -1,12 +1,14 @@
 
-// Configuração do Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBKtKrRP4AjLHcdeEdyTlTZC9hHdv5y7no",
+  authDomain: "metodo-forja.firebaseapp.com",
+  databaseURL: "https://metodo-forja-default-rtdb.firebaseio.com",
+  projectId: "metodo-forja",
+  storageBucket: "metodo-forja.firebasestorage.app",
+  messagingSenderId: "949719386034",
+  appId: "1:949719386034:web:5c015b5fbe082821ea86ab",
+  measurementId: "G-6Z9EJLY5LZ"
 };
 
 // Inicializa o Firebase
@@ -22,7 +24,7 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(() => {
       // Redireciona após login bem-sucedido
-      window.location.href = "https://metodo-forja-mf.github.io/modulos/";
+      window.location.href = "https://metodo-forja-mf.github.io/membros/";
     })
     .catch((error) => {
       alert("Erro no login: " + error.message);
